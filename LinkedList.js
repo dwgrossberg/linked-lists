27177c4fa19c7e5219c90152980e50b2c4972558
Daniel <dwgrossberg@gmail.com>
2022-08-23 16:17:27 -0400

    Add LinkedList basic outline

--- LinkedList.js
+++ LinkedList.js
@@ -1,3 +1,25 @@
-const LinkedList = () => {};
+import Node from "../linked-lists/Node";
+
+const LinkedList = () => {
+  let HEAD = null;
+  let length = 0;
+
+  const append = (value) => {
+    const node = Node(value);
+    length += 1;
+    if (HEAD === null) {
+      return (HEAD = node);
+    }
+  };
+
+  const head = () => {
+    return HEAD.value;
+  };
+
+  return {
+    append,
+    head,
+  };
+};
 
 export default LinkedList;
--- LinkedList.test.js
+++ LinkedList.test.js
@@ -1,8 +1,16 @@
 /* eslint-disable no-undef */
 import LinkedList from "../linked-lists/LinkedList";
+import Node from "../linked-lists/Node";
 
-describe("List methods", () => {
+const node1 = Node("node1");
+const list = LinkedList();
+
+describe("LinkedList methods", () => {
   test("happy path", () => {
     expect(LinkedList);
+  });
+  test("append(value) adds a new node containing value to the end of the list", () => {
+    list.append(node1);
+    expect(list.head()).toBe("node1");
   });
 });
